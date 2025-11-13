d648b0fd3ee57f218219eacbd6da98e3f2db6f57
3ka1tz <ekaitzlopez2025@gmail.com>
2025-11-13 11:49:38 +0100

    ex01 Makefile and main.cpp were improved

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,16 +4,16 @@
 
 int main() {
     Cat originalCat;
-    originalCat.setIdea(0, "I want fish");
-    originalCat.setIdea(1, "I want milk");
+    originalCat.setIdea(0, "Old idea0");
+    originalCat.setIdea(1, "Old idea1");
 
     Cat copiedCat = originalCat;
-    copiedCat.setIdea(0, "I want tuna");
+    copiedCat.setIdea(0, "New idea0");
 
-    std::cout << "Original Cat Idea[0]: " << originalCat.getIdea(0) << std::endl;
-    std::cout << "Original Cat Idea[1]: " << originalCat.getIdea(1) << std::endl;
-    std::cout << "Copied   Cat Idea[0]: " << copiedCat.getIdea(0) << std::endl;
-    std::cout << "Copied   Cat Idea[1]: " << copiedCat.getIdea(1) << std::endl;
+    std::cout << "originalCat Idea[0]: " << originalCat.getIdea(0) << std::endl;
+    std::cout << "originalCat Idea[1]: " << originalCat.getIdea(1) << std::endl;
+    std::cout << "copiedCat   Idea[0]: " << copiedCat.getIdea(0) << std::endl;
+    std::cout << "copiedCat   Idea[1]: " << copiedCat.getIdea(1) << std::endl;
 
     return 0;
 }
